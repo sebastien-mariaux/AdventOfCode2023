92e2272605a9da301e0ea8a8e6b2873833f26273
Sébastien Mariaux <smariaux@regate.io>
2023-12-02 14:10:01 +0100

    day 2 part 2

--- day02/part2/src/main.rs
+++ day02/part2/src/main.rs
@@ -1,12 +1,28 @@
-use std::fs;
+use std::{collections::HashMap, fs};
 
 fn main() {
-    let result = solve_puzzle("input");
+    let result = solve_puzzle("../input");
     println!("And the result is {}", result);
 }
 
 fn solve_puzzle(file_name: &str) -> u32 {
-    0
+    let data = read_data(file_name);
+    data.lines().map(|game| get_the_powaa(game)).sum::<u32>()
+}
+
+fn get_the_powaa(game: &str) -> u32 {
+    let mut mins = HashMap::from([("red", 0), ("green", 0), ("blue", 0)]);
+    for subgame in game.split(": ").nth(1).unwrap().split("; ") {
+        for cube in subgame.split(", ") {
+            let mut test = cube.split(' ');
+            let count = test.next().unwrap().parse::<u32>().unwrap();
+            let color = test.next().unwrap();
+            if mins.get(color).unwrap() < &count {
+                mins.insert(color, count);
+            }
+        }
+    }
+    mins.values().fold(1, |acc, f| acc * f)
 }
 
 fn read_data(file_name: &str) -> String {
@@ -19,12 +35,11 @@ mod test {
 
     #[test]
     fn test_example_data() {
-        assert_eq!(0, solve_puzzle("test_data"));
+        assert_eq!(2286, solve_puzzle("test_data"));
     }
 
     #[test]
-    #[ignore]
     fn test_solution() {
-        assert_eq!(0, solve_puzzle("../input"));
+        assert_eq!(83707, solve_puzzle("../input"));
     }
 }
