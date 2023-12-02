b1f3f2cc50e1a5196c57020d68645bfe08954767
Sébastien Mariaux <smariaux@regate.io>
2023-12-02 13:56:22 +0100

    Day 02 - part 1

--- day02/part1/src/main.rs
+++ day02/part1/src/main.rs
@@ -1,17 +1,69 @@
-use std::fs;
+use std::{collections::HashMap, fs};
 
 fn main() {
-    let result = solve_puzzle("input");
+    let result = solve_puzzle("../input");
     println!("And the result is {}", result);
 }
 
 fn solve_puzzle(file_name: &str) -> u32 {
-    0
+    let data = read_data(file_name);
+    let totals = HashMap::from([("red", 12), ("green", 13), ("blue", 14)]);
+    data.lines()
+        .enumerate()
+        .map(|(i, game)| index_if_game_possible(i, game, &totals))
+        .sum::<u32>()
+}
+
+fn index_if_game_possible(i: usize, game: &str, totals: &HashMap<&str, u32>) -> u32 {
+    for subgame in game.split(": ").nth(1).unwrap().split("; ") {
+        for cube in subgame.split(", ") {
+            let mut test = cube.split(' ');
+            let count = test.next().unwrap().parse::<u32>().unwrap();
+            let color = test.next().unwrap();
+            if totals.get(color).unwrap() < &count {
+                return 0;
+            }
+        }
+    }
+    i as u32 + 1
 }
 
 fn read_data(file_name: &str) -> String {
     fs::read_to_string(file_name).expect("Error")
 }
+
+// subgame
+// .split(", ")
+// .map(|cube| {
+//     let mut test = cube.split(" ");
+//     let count = test.next().unwrap().parse::<u32>().unwrap();
+//     let color = test.next().unwrap();
+//     stuff.insert(color, count);
+// })
+
+// let formatted_data = data
+//         .lines()
+//         .map(|game| {
+//             let mut game_possible = true;
+//             game.split(": ")
+//                 .nth(1)
+//                 .unwrap()
+//                 .split("; ")
+//                 .map(|subgame| {
+//                     let mut stuff = HashMap::new();
+//                     for cube in subgame.split(", ") {
+//                         let mut test = cube.split(" ");
+//                         let count = test.next().unwrap().parse::<u32>().unwrap();
+//                         let color = test.next().unwrap();
+
+//                         stuff.insert(color, count);
+//                     }
+//                     stuff
+//                 })
+//                 .collect::<Vec<_>>()
+//         })
+//         .collect::<Vec<_>>();
+//     println!("{:?}", formatted_data);
 
 #[cfg(test)]
 mod test {
@@ -19,12 +71,14 @@ mod test {
 
     #[test]
     fn test_example_data() {
-        assert_eq!(0, solve_puzzle("test_data"));
+        assert_eq!(8, solve_puzzle("test_data"));
     }
 
     #[test]
-    #[ignore]
     fn test_solution() {
-        assert_eq!(0, solve_puzzle("../input"));
+        assert_eq!(2685, solve_puzzle("../input"));
     }
 }
+
+// red, green, or blue
+// only 12 red cubes, 13 green cubes, and 14 blue cubes?
