8e6325ed69fabaa38e711d2d8154d3134f39f4ae
Sébastien Mariaux <smariaux@regate.io>
2023-12-06 07:28:32 +0100

    part 2

--- day06/part2/src/main.rs
+++ day06/part2/src/main.rs
@@ -1,12 +1,45 @@
 use std::fs;
 
 fn main() {
-    let result = solve_puzzle("input");
+    let result = solve_puzzle("../input");
     println!("And the result is {}", result);
 }
 
-fn solve_puzzle(file_name: &str) -> u32 {
-    0
+fn solve_puzzle(file_name: &str) -> usize {
+    let data = read_data(file_name);
+    let mut lines = data.lines();
+    let time = lines
+        .next()
+        .unwrap()
+        .split(':')
+        .nth(1)
+        .unwrap()
+        .split_whitespace()
+        .collect::<Vec<&str>>()
+        .join("")
+        .parse::<usize>()
+        .unwrap();
+
+    let distance = lines
+        .next()
+        .unwrap()
+        .split(':')
+        .nth(1)
+        .unwrap()
+        .split_whitespace()
+        .collect::<Vec<&str>>()
+        .join("")
+        .parse::<usize>()
+        .unwrap();
+
+    let first = (0..time + 1)
+        .find(|push_time| {
+            let travelled = (time - push_time) * push_time;
+            travelled > distance
+        })
+        .unwrap();
+    let last = time - first;
+    last - first + 1
 }
 
 fn read_data(file_name: &str) -> String {
@@ -19,12 +52,12 @@ mod test {
 
     #[test]
     fn test_example_data() {
-        assert_eq!(0, solve_puzzle("test_data"));
+        assert_eq!(71503, solve_puzzle("../test_data"));
     }
 
     #[test]
-    #[ignore]
+    // #[ignore]
     fn test_solution() {
-        assert_eq!(0, solve_puzzle("../input"));
+        assert_eq!(35961505, solve_puzzle("../input"));
     }
 }
