edbd13ba12857ad18be8d5d013ba729dd53e46bc
Sébastien Mariaux <smariaux@regate.io>
2023-12-08 12:10:45 +0100

    Day 8

--- day08/part1/src/main.rs
+++ day08/part1/src/main.rs
@@ -1,11 +1,43 @@
-use std::fs;
+use std::{fs, collections::HashMap};
 
 fn main() {
-    let result = solve_puzzle("input");
+    let result = solve_puzzle("../input");
     println!("And the result is {}", result);
 }
 
 fn solve_puzzle(file_name: &str) -> u32 {
+    let data = read_data(file_name);
+    let mut lines = data.lines();
+    let directions = lines.next().unwrap();
+    lines.next();
+
+    let mut maps =  HashMap::new();
+    for  line in lines {
+        if line.len() == 0 {
+            break;
+        }
+        let fixed_line = line.replace("(", "").replace(")", "");
+        let (source, destinations) = fixed_line.split_once(" = ").unwrap();
+;        let (dest_left, dest_right) = destinations.split_once(", ").unwrap();
+        maps.insert(source.to_string(), (dest_left.to_string(), dest_right.to_string()));
+    }
+
+    let mut position = String::from("AAA");
+
+    println!("{:?}", maps);
+    for (step, direction) in directions.chars().cycle().enumerate() {
+        println!("{}",position);
+        let (left, right) = maps.get(&position).unwrap();
+        if direction == 'L' {
+            position = left.to_string();
+        } else {
+            position = right.to_string();
+        }
+        if position == "ZZZ" {
+            return step as u32 + 1;
+        }
+    }
+
     0
 }
 
@@ -19,12 +51,12 @@ mod test {
 
     #[test]
     fn test_example_data() {
-        assert_eq!(0, solve_puzzle("test_data"));
+        assert_eq!(6, solve_puzzle("test_data"));
     }
 
     #[test]
-    #[ignore]
+    // #[ignore]
     fn test_solution() {
-        assert_eq!(0, solve_puzzle("../input"));
+        assert_eq!(12083, solve_puzzle("../input"));
     }
 }
--- day08/part2/src/main.rs
+++ day08/part2/src/main.rs
@@ -1,13 +1,65 @@
-use std::fs;
+use std::{fs, collections::HashMap};
+use num::integer::lcm;
 
 fn main() {
-    let result = solve_puzzle("input");
+    let result = solve_puzzle("../input");
     println!("And the result is {}", result);
 }
 
-fn solve_puzzle(file_name: &str) -> u32 {
-    0
+fn solve_puzzle(file_name: &str) -> usize {
+    let data = read_data(file_name);
+    let mut lines = data.lines();
+    let directions = lines.next().unwrap();
+    lines.next();
+
+    let mut maps =  HashMap::new();
+    for  line in lines {
+        if line.len() == 0 {
+            break;
+        }
+        let fixed_line = line.replace("(", "").replace(")", "");
+        let (source, destinations) = fixed_line.split_once(" = ").unwrap();
+        let (dest_left, dest_right) = destinations.split_once(", ").unwrap();
+        maps.insert(source.to_string(), (dest_left.to_string(), dest_right.to_string()));
+    }
+    let mut positions = maps.keys().filter(|x| x.chars().last().unwrap() == 'A').collect::<Vec<_>>();
+
+    let mut cycles: HashMap<usize, Vec<usize>> = HashMap::new();
+    for i in 0..positions.len() {
+        cycles.insert(i, vec![]);
+    }
+
+    for (step, direction) in directions.chars().cycle().enumerate() {
+        positions = positions.iter().enumerate().map(|(index, position)| {
+            let (left, right) = maps.get(*position).unwrap();
+            let next_position = if direction == 'L' {
+                left
+            } else {
+                right
+            };
+            if next_position.chars().last().unwrap() == 'Z' {
+                cycles.get_mut(&index).unwrap().push(step);
+            }
+            next_position
+        }).collect::<Vec<&String>>();
+
+        if cycles.values().all(|x| x.len() >= 2) {
+            break;
+        }
+    }
+
+    let cycle_lengths = cycles.values().map(|x| x[1] - x[0]).collect::<Vec<_>>();
+
+    // Compute Lowest common denominator of the elements in the vector
+    let mut result = cycle_lengths[0];
+    for i in 1..cycle_lengths.len() {
+        result = lcm(result, cycle_lengths[i]);
+    }
+
+    result
 }
+
+
 
 fn read_data(file_name: &str) -> String {
     fs::read_to_string(file_name).expect("Error")
@@ -19,12 +71,11 @@ mod test {
 
     #[test]
     fn test_example_data() {
-        assert_eq!(0, solve_puzzle("test_data"));
+        assert_eq!(6, solve_puzzle("test_data"));
     }
 
     #[test]
-    #[ignore]
     fn test_solution() {
-        assert_eq!(0, solve_puzzle("../input"));
+        assert_eq!(13385272668829, solve_puzzle("../input"));
     }
 }
