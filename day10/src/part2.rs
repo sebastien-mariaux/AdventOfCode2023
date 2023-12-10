a94c2069df1ff8c6b515f8764820a9432ba2cba6
Sébastien Mariaux <smariaux@regate.io>
2023-12-10 12:04:06 +0100

    day 10 part 1

--- day10/src/part2.rs
+++ day10/src/part2.rs
@@ -1,5 +1,4 @@
 use crate::utils::read_data;
-use num::integer::lcm;
 use std::{collections::HashMap, fs};
 
 pub fn solve_puzzle(file_name: &str) -> usize {
@@ -13,6 +12,7 @@ mod test {
     use super::*;
 
     #[test]
+    #[ignore]
     fn test_example_data() {
         assert_eq!(0, solve_puzzle("test_data"));
     }
