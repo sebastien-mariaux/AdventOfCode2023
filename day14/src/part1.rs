22e6fad746bd8448e3aa790bf83500081f142514
Sébastien Mariaux <smariaux@regate.io>
2023-12-14 11:37:36 +0100

    day 14 - part 2

--- day14/src/part1.rs
+++ day14/src/part1.rs
@@ -1,11 +1,9 @@
-use std::collections::HashSet;
-
 use crate::utils::read_data;
 
 pub fn solve_puzzle(file_name: &str) -> usize {
     let data = read_data(file_name);
 
-    let mut plateform = data.lines().map(|l | l.chars().collect::<Vec<char>>()).collect::<Vec<Vec<char>>>();
+    let plateform = data.lines().map(|l | l.chars().collect::<Vec<char>>()).collect::<Vec<Vec<char>>>();
     let tilted_north = tilt_north(plateform);
 
 
@@ -24,33 +22,28 @@ fn tilt_north(plateform: Vec<Vec<char>>) -> Vec<Vec<char>> {
         for (j,  char) in row.iter().enumerate() {
             if char == &'O' {
                 let new_position = get_new_position(&plateform, i, j);
-                println!("{} {} -> {} {}", i, j, new_position.0, new_position.1);
                 tilted_north[new_position.0][new_position.1] = 'O';
             } else if char == &'#' {
                 tilted_north[i][j] = '#';
             }
         }
     }
-    for row in tilted_north.iter() {
-        println!("{:?}", row);
-    }
+
     tilted_north
 }
 
-fn get_new_position(plateform: &Vec<Vec<char>>, i: usize, j:usize) -> (usize, usize) {
+fn get_new_position(plateform: &[Vec<char>], i: usize, j:usize) -> (usize, usize) {
     let current_col = plateform.iter().map(|row| row[j]).collect::<Vec<char>>();
-    println!("{}", current_col[0..i].iter().collect::<String>());
     let closest_cube = current_col[0..i].iter().enumerate().filter(|(_, c)| c == &&'#')
     .map(|(ix, _)| ix).max();
     match closest_cube {
         Some(pos) => {
-            println!("closest cube {}", pos);
             let rounded_below = current_col[pos..i].iter().filter(|c| c == &&'O').count();
-            return (pos + rounded_below + 1, j)
+            (pos + rounded_below + 1, j)
         },
         None => {
             let rounded_below = current_col[0..i].iter().filter(|c| c == &&'O').count();
-            return (rounded_below, j)
+            (rounded_below, j)
         }
     }
 }
@@ -65,8 +58,7 @@ mod test {
     }
 
     #[test]
-    // #[ignore]
     fn test_solution() {
-        assert_eq!(0, solve_puzzle("input"));
+        assert_eq!(102497, solve_puzzle("input"));
     }
 }
