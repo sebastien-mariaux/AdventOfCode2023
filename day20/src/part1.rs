5f3b8fbcde46a88f00c95482c97203dae1262e77
Sébastien Mariaux <smariaux@regate.io>
2023-12-20 22:05:00 +0100

    Day20 - part 1

--- day20/src/part1.rs
+++ day20/src/part1.rs
@@ -1,14 +1,14 @@
 use crate::utils::read_data;
 use std::collections::VecDeque;
-use std::{collections::HashMap, path::Display};
 use std::fmt;
+use std::collections::HashMap;
 
 struct Module {
     mod_type: char,
     name: String,
     on: bool,
     received_pulses: HashMap<String, char>,
-    targets: Vec::<String>,
+    targets: Vec<String>,
     low_pulse_count: usize,
     high_pulse_count: usize,
 }
@@ -30,16 +30,23 @@ impl Module {
         self.on = !self.on;
     }
 
-    fn is_on(&self) -> bool {
-        self.on
-    }
-
     fn is_off(&self) -> bool {
         !self.on
     }
 
-    fn receive_pulse(&mut self, pulse: char, sender: String) {
-        self.received_pulses.insert(sender, pulse);
+    fn initiate_input_modules(&mut self, modules: &HashMap<String, Vec<String>>) {
+        if self.mod_type != '&' {
+            return;
+        }
+        for (name, targets) in modules.iter() {
+            if targets.contains(&self.name) {
+                self.receive_pulse('L', name);
+            }
+        }
+    }
+
+    fn receive_pulse(&mut self, pulse: char, sender: &String) {
+        self.received_pulses.insert(sender.clone(), pulse);
     }
 
     fn increase_count(&mut self, pulse: char) {
@@ -50,46 +57,52 @@ impl Module {
         }
     }
 
-    // fn send_pulse(&mut self, pulse: char, target: String, modules: &mut HashMap<String, Module>) {
-    //     let target_module = modules.get_mut(&target).unwrap();
-    //     target_module.receive_pulse(pulse, self.name.to_string());
-    // }
+    fn send_pulse(
+        &mut self,
+        pulse: char,
+        target: String,
+        stack: &mut VecDeque<(String, char)>,
+        sent_pulses: &mut Vec<(String, char)>,
+    ) {
+        stack.push_back((target.to_string(), pulse));
+        sent_pulses.push((target.to_string(), pulse));
+        self.increase_count(pulse);
+        println!("{} -> {} ({})", self.name, target, pulse);
+    }
 
-    fn process_pulse(&mut self, stack: &mut VecDeque::<(String, char)>, received_pulse: char) {
-        if self.name == "button"  {
-            stack.push_back(("broadcaster".to_string(), 'L'));
-            self.increase_count('L');
-        } else  if self.name == "broadcaster"  {
+    fn process_pulse(
+        &mut self,
+        stack: &mut VecDeque<(String, char)>,
+        received_pulse: char,
+    ) -> Vec<(String, char)> {
+        let mut sent_pulses = Vec::<(String, char)>::new();
+        if self.name == "button" {
+            self.send_pulse('L', "broadcaster".to_string(), stack, &mut sent_pulses);
+        } else if self.name == "broadcaster" {
             for target in self.targets.clone() {
-                stack.push_back((target.to_string(), received_pulse));
-                // self.send_pulse(received_pulse, target.to_string(), modules);
-                self.increase_count(received_pulse);
+                self.send_pulse(received_pulse, target.to_string(), stack, &mut sent_pulses);
             }
-        } else  if self.mod_type == '%' {
+        } else if self.mod_type == '%' {
             if received_pulse == 'L' {
                 for target in self.targets.clone() {
                     let next_pulse = if self.is_off() { 'H' } else { 'L' };
-                    stack.push_back((target.to_string(), next_pulse));
-                    // self.send_pulse(next_pulse, target.to_string(), modules);
-                    self.increase_count(next_pulse);
+                    self.send_pulse(next_pulse, target.to_string(), stack, &mut sent_pulses);
                 }
                 self.toggle();
             }
         } else if self.mod_type == '&' {
-            if self.received_pulses.values().all(|x |x == &'H') {
+            println!("Received pulses: {:?}", self.received_pulses);
+            if self.received_pulses.values().all(|x| x == &'H') {
                 for target in self.targets.clone() {
-                    stack.push_back((target.to_string(), 'L'));
-                    // self.send_pulse('L', target.to_string(), modules);
-                    self.increase_count('L');
+                    self.send_pulse('L', target.to_string(), stack, &mut sent_pulses);
                 }
             } else {
                 for target in self.targets.clone() {
-                    stack.push_back((target.to_string(), 'H'));
-                    // self.send_pulse('H', target.to_string(), modules);
-                    self.increase_count('H');
+                    self.send_pulse('H', target.to_string(), stack, &mut sent_pulses);
                 }
             }
         }
+        sent_pulses
     }
 }
 
@@ -102,35 +115,72 @@ impl fmt::Display for Module {
 pub fn solve_puzzle(file_name: &str) -> usize {
     let data = read_data(file_name);
 
+    // Build modules list from input
     let mut modules = HashMap::<String, Module>::new();
-    for  line in data.lines() {
-        let ( a, b) = line.split_once(" -> ").unwrap();
-        let targets = b.split(", ").map(|x| x.to_string()).collect::<Vec<String>>();
+    for line in data.lines() {
+        let (a, b) = line.split_once(" -> ").unwrap();
+        let targets = b
+            .split(", ")
+            .map(|x| x.to_string())
+            .collect::<Vec<String>>();
         if a == "broadcaster" {
             modules.insert(a.to_string(), Module::new('B', a.to_string(), targets));
         } else {
             let (mod_type, name) = a.split_at(1);
-            modules.insert(name.to_string(), Module::new(mod_type.chars().next().unwrap(), name.to_string(), targets));
+            modules.insert(
+                name.to_string(),
+                Module::new(mod_type.chars().next().unwrap(), name.to_string(), targets),
+            );
         }
     }
-    modules.insert("button".to_string(), Module::new('S', "button".to_string(), vec!["broadcaster".to_string()]));
+    // Add button in  modules
+    modules.insert(
+        "button".to_string(),
+        Module::new('S', "button".to_string(), vec!["broadcaster".to_string()]),
+    );
 
+    // initiate received pulses for modules
+    let mut modules_targets = HashMap::new();
+    for module in modules.values() {
+        modules_targets.insert(module.name.to_string(), module.targets.clone());
+    }
+    for module in modules.values_mut() {
+        module.initiate_input_modules(&modules_targets);
+    }
+
+    // Initiate stack
     let mut stack = VecDeque::<(String, char)>::new();
-    for _  in 0..1000 {
-        stack.push_back(("button".to_string(), 'L'));
-    }
 
-    while !stack.is_empty() {
-        let (name, pulse) = stack.pop_front().unwrap();
-        if let Some(module) = &mut modules.get_mut(&name) {
-            module.process_pulse(&mut stack, pulse);
+    // Process stack 1000 times
+    for _ in 0..1000 {
+        stack.push_back(("button".to_string(), 'L'));
+        while !stack.is_empty() {
+            let (name, pulse) = stack.pop_front().unwrap();
+            let module = &mut modules.get_mut(&name);
+            match module {
+                Some(module) => {
+                    let sent_pulses = module.process_pulse(&mut stack, pulse);
+                    for (target, pulse) in sent_pulses {
+                        println!("debug {} -> {} ({})", name, target, pulse);
+                        let target_module = modules.get_mut(&target);
+                        if let Some(target_module) = target_module {
+                            target_module.receive_pulse(pulse, &name);
+                        }
+                    }
+                }
+                None => {
+                    println!("{} not found", name);
+                }
+            }
         }
     }
 
+    // Counting results
     let high_pulses_count: usize = modules.values().map(|x| x.high_pulse_count).sum();
     let low_pulses_count: usize = modules.values().map(|x| x.low_pulse_count).sum();
 
-
+    println!("High pulses: {}", high_pulses_count);
+    println!("Low pulses: {}", low_pulses_count);
     high_pulses_count * low_pulses_count
 }
 
@@ -143,7 +193,6 @@ mod test {
         assert_eq!(32000000, solve_puzzle("test_data_1"));
     }
 
-
     #[test]
     // #[ignore]
     fn test_example_data_2() {
@@ -151,9 +200,9 @@ mod test {
     }
 
     #[test]
-    #[ignore]
+    // #[ignore]
     fn test_solution() {
-        assert_eq!(0, solve_puzzle("input"));
+        assert_eq!(812609846, solve_puzzle("input"));
     }
 }
 
