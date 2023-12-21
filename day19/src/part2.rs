a1cf586ff16697e65ea8a52ac9b7e9d6f74f364f
Sébastien Mariaux <smariaux@regate.io>
2023-12-22 00:28:22 +0100

    Day 19 - part 2

--- day19/src/part2.rs
+++ day19/src/part2.rs
@@ -1,12 +1,11 @@
 use std::collections::HashMap;
-use std::collections::HashSet;
 
 use crate::utils::read_data;
 
 pub fn solve_puzzle(file_name: &str) -> usize {
     let data = read_data(file_name);
 
-    let (part1, part2) = data
+    let (part1, _) = data
         .split_once("\n\n")
         .map(|(x, y)| (x.to_string(), y.to_string()))
         .unwrap();
@@ -27,7 +26,14 @@ pub fn solve_puzzle(file_name: &str) -> usize {
         &mut accepted_ranges,
     );
 
-    0
+    accepted_ranges
+        .iter()
+        .map(get_combinations_count)
+        .sum()
+}
+
+fn get_combinations_count(ranges: &HashMap<char, (usize, usize)>) -> usize {
+    ranges.values().map(|range| range.1 - range.0).product()
 }
 
 fn process_ranges(
@@ -36,14 +42,52 @@ fn process_ranges(
     name: String,
     accepted_ranges: &mut Vec<HashMap<char, (usize, usize)>>,
 ) {
+    if name == "R" {
+        return;
+    }
+    if name == "A" {
+        accepted_ranges.push(ranges.clone());
+        return;
+    }
     let conditions = workflows.get(&name).unwrap();
+    // println!("conditions {:?}", conditions);
     for condition in conditions {
         if condition == "R" {
             continue;
         }
         if condition == "A" {
             accepted_ranges.push(ranges.clone());
+            continue;
         }
+        if !condition.contains('>') && !condition.contains('<') {
+            process_ranges(workflows, ranges, condition.to_string(), accepted_ranges);
+        }
+
+        if condition.contains('<') {
+            let (key, value) = condition
+                .split_once('<')
+                .map(|(x, y)| {
+                    (
+                        x.chars().next().unwrap(),
+                        y.split(':').next().unwrap().parse::<usize>().unwrap(),
+                    )
+                })
+                .unwrap();
+            let current_range_for_char = ranges.get(&key).unwrap();
+            let range_in_condition = (current_range_for_char.0, value);
+            let remaining_range = (value, current_range_for_char.1);
+
+            let mut new_ranges = ranges.clone();
+            new_ranges.insert(key, range_in_condition);
+            process_ranges(
+                workflows,
+                &mut new_ranges,
+                condition.split_once(':').unwrap().1.to_string(),
+                accepted_ranges,
+            );
+            ranges.insert(key, remaining_range);
+        }
+
         if condition.contains('>') {
             let (key, value) = condition
                 .split_once('>')
@@ -55,28 +99,29 @@ fn process_ranges(
                 })
                 .unwrap();
             let current_range_for_char = ranges.get(&key).unwrap();
-            // let mut new_ranges = HashMap::new();
+            let range_in_condition = (value + 1, current_range_for_char.1);
+            let remaining_range = (current_range_for_char.0, value + 1);
+
+            let mut new_ranges = ranges.clone();
+            new_ranges.insert(key, range_in_condition);
+            process_ranges(
+                workflows,
+                &mut new_ranges,
+                condition.split_once(':').unwrap().1.to_string(),
+                accepted_ranges,
+            );
+            ranges.insert(key, remaining_range);
         }
     }
 }
 
-// fn format_part(part: &str) -> HashMap<char, usize> {
-//     part[1..part.len() - 1]
-//         .split(',')
-//         .map(|x| {
-//             let (key, value) = x.split_once('=').unwrap();
-//             (key.chars().next().unwrap(), value.parse::<usize>().unwrap())
-//         })
-//         .collect()
-// }
-
-fn format_workflows(workflows: &String) -> HashMap<String, Vec<String>> {
+fn format_workflows(workflows: &str) -> HashMap<String, Vec<String>> {
     let mut all_workflows: HashMap<String, Vec<String>> = HashMap::new();
     for mut workflow in workflows.lines() {
         let mut chars = workflow.chars();
         chars.next_back();
         workflow = chars.as_str();
-        let (name, conditions) = workflow.split_once("{").unwrap();
+        let (name, conditions) = workflow.split_once('{').unwrap();
         all_workflows.insert(
             name.to_string(),
             conditions.split(',').map(|x| x.to_string()).collect(),
@@ -85,103 +130,17 @@ fn format_workflows(workflows: &String) -> HashMap<String, Vec<String>> {
     all_workflows
 }
 
-fn is_accepted(part: &HashMap<char, usize>, workflows: &HashMap<String, Vec<String>>) -> bool {
-    // apply_rule(part, workflows, String::from("in"))
-    false
-}
-
-// fn apply_rule(
-//     part: &HashMap<char, usize>,
-//     workflows: &HashMap<String, Vec<String>>,
-//     name: String,
-// ) -> bool {
-//     if name == "R" {
-//         return false;
-//     }
-//     if name == "A" {
-//         return true;
-//     }
-//     let rule = workflows.get(&name).unwrap().clone();
-//     for condition in rule.iter() {
-//         if condition == "R"  {
-//             return false;
-//         }
-//         if condition == "A"  {
-//             return true;
-//         }
-//         if condition.contains('>')  {
-//             let (key, value) = condition
-//                 .split_once('>')
-//                 .map(|(x, y)| {
-//                     (
-//                         x.chars().next().unwrap(),
-//                         y.split(':').next().unwrap().parse::<usize>().unwrap(),
-//                     )
-//                 })
-//                 .unwrap();
-//             if part.get(&key).unwrap() > &value {
-//                 return apply_rule(
-//                     part,
-//                     workflows,
-//                     condition.split_once(':').unwrap().1.to_string(),
-//                 );
-//             } else {
-//                 continue;
-//             }
-//         }
-//         if condition.contains('<') {
-//             let (key, value) = condition
-//                 .split_once('<')
-//                 .map(|(x, y)| {
-//                     (
-//                         x.chars().next().unwrap(),
-//                         y.split(':').next().unwrap().parse::<usize>().unwrap(),
-//                     )
-//                 })
-//                 .unwrap();
-//             if part.get(&key).unwrap() < &value {
-//                 return apply_rule(
-//                     part,
-//                     workflows,
-//                     condition.split_once(':').unwrap().1.to_string(),
-//                 );
-//             } else {
-//                 continue;
-//             }
-//         }
-//         println!("condition {:?}", condition);
-
-//         return apply_rule(part, workflows, condition.to_string());
-//     }
-
-//     false
-// }
-
 #[cfg(test)]
 mod test {
     use super::*;
 
     #[test]
-    #[ignore]
     fn test_example_data() {
         assert_eq!(167409079868000, solve_puzzle("test_data"));
     }
 
     #[test]
-    #[ignore]
     fn test_solution() {
-        assert_eq!(0, solve_puzzle("input"));
+        assert_eq!(124167549767307, solve_puzzle("input"));
     }
 }
-
-// machine parts
-// x: Extremely cool looking
-// m: Musical (it makes a noise when you hit it)
-// a: Aerodynamic
-// s: Shiny
-
-// workflow : name + rules
-// rejected (R)
-// accepted (A).
-
-// in =  initial workflow
--- day21/src/main.rs
+++ day21/src/main.rs
@@ -1,5 +1,5 @@
 mod part1;
-// mod part2;
+mod part2;
 mod utils;
 
 fn main() {
@@ -7,9 +7,9 @@ fn main() {
         "The result for part 1 is {}",
         part1::solve_puzzle("input", 64)
     );
-    // println!(
-    //     "The result for part 2 is {}",
-    //     part2::solve_puzzle("test_data", 100)
-    // );
-    // println!("The result for part 2 is {}", part2::solve_puzzle("input", 26501365));
+    println!(
+        "The result for part 2 is {}",
+        part2::solve_puzzle("test_data", 100)
+    );
+    println!("The result for part 2 is {}", part2::solve_puzzle("input", 26501365));
 }
