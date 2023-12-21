9ee6645d3d4956c2447d7a60970f8e8faaceed70
Sébastien Mariaux <smariaux@regate.io>
2023-12-21 07:51:47 +0100

    day 21 - 1

--- day21/src/main.rs
+++ day21/src/main.rs
@@ -3,6 +3,6 @@ mod part2;
 mod utils;
 
 fn main() {
-    println!("The result for part 1 is {}", part1::solve_puzzle("input"));
+    println!("The result for part 1 is {}", part1::solve_puzzle("input", 64));
     println!("The result for part 2 is {}", part2::solve_puzzle("input"));
 }
