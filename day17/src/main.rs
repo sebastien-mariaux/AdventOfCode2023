3695a8fa85084dda77c3af1f9c9ff98a0f756b67
Sébastien Mariaux <smariaux@regate.io>
2023-12-22 23:23:42 +0100

    Rename + snall fixes

--- day17/src/main.rs
+++ day17/src/main.rs
@@ -1,13 +1,13 @@
 mod part1;
-mod part1_unoptimized;
+mod part1_first_try;
 mod part2;
 mod utils;
 
 fn main() {
     println!("The result for part 1 is {}", part1::solve_puzzle("input"));
     println!(
-        "The result for part 1 (on test data - without optim) is {}",
-        part1_unoptimized::solve_puzzle("test_data")
+        "The result for part 1 (on test data - First try) is {}",
+        part1_first_try::solve_puzzle("test_data")
     );
     println!("The result for part 2 is {}", part2::solve_puzzle("input"));
 }
